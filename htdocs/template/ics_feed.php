63e0cdd4de03cf1135a1b941c24017e4e5120e54
Ale Rimoldi <ale@graphicslab.org>
2014-09-23 20:51:01 +0200

    shuffle around the calendar properties

--- htdocs/template/ics_feed.php
+++ htdocs/template/ics_feed.php
@@ -1,10 +1,11 @@
 BEGIN:VCALENDAR
-X-WR-CALNAME:Trainings beim BC-Oberurdorf
-X-WR-TIMEZONE:UTC
-X-WR-CALDESC:Trainings beim BC-Oberurdorf
 VERSION:2.0
 PRODID:-//hacksw/handcal//NONSGML v1.0//EN
 CALSCALE:GREGORIAN
+METHOD:PUBLISH
+X-WR-CALNAME:Trainings beim BC-Oberurdorf
+X-WR-TIMEZONE:UTC
+X-WR-CALDESC:Trainings beim BC-Oberurdorf
 <?php foreach ($event as $item) : ?>
 BEGIN:VEVENT
 DTEND:<?= $item['end'] ?>
